1cd233b2c24e47cff1f8aa4a60bd675fa8eb417f
Karthik-Yellapragada <karthikyellapragada601@gmail.com>
2024-04-21 12:16:20 +0530

    deleting

--- assistant/new_project/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-# myapp/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('voice_assistant/', views.voice_assistant, name='voice_assistant'),
-]
